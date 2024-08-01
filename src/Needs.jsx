90a5757a1fb4d7e4e588b8bb329e8ae496add4d3
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-08-01 12:10:15 +0200

    Implement one more component and practice jsx markups

---
+++ src/Needs.jsx
@@ -0,0 +1,3 @@
+export function Needs() {
+  return <h2>My Needs</h2>;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import Greeting from "./Greeting.jsx";
 import "./index.css";
+import { Needs } from "./Needs.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Greeting />
+    <Needs />
   </React.StrictMode>
 );
