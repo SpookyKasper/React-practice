2b48c3dea8c8c31a62d1ba2a8918850c5af5a102
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-08-05 12:08:21 +0200

    Extract component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,57 @@
-import { Fragment } from "react";
+import { getImageUrl } from "./utils.js";
 
-const poem = {
-  lines: [
-    "I write, erase, rewrite",
-    "Erase again, and then",
-    "A poppy blooms.",
-  ],
-};
-
-const verseComponent = (line, index) => {
+export default function Gallery() {
   return (
-    <Fragment key={index}>
-      {index > 0 && <hr />}
-      <p key={index}>{line}</p>
-    </Fragment>
+    <div>
+      <h1>Notable Scientists</h1>
+      <section className="profile">
+        <h2>Maria Skłodowska-Curie</h2>
+        <img
+          className="avatar"
+          src={getImageUrl("szV5sdG")}
+          alt="Maria Skłodowska-Curie"
+          width={70}
+          height={70}
+        />
+        <ul>
+          <li>
+            <b>Profession: </b>
+            physicist and chemist
+          </li>
+          <li>
+            <b>Awards: 4 </b>
+            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal,
+            Matteucci Medal)
+          </li>
+          <li>
+            <b>Discovered: </b>
+            polonium (chemical element)
+          </li>
+        </ul>
+      </section>
+      <section className="profile">
+        <h2>Katsuko Saruhashi</h2>
+        <img
+          className="avatar"
+          src={getImageUrl("YfeOqp2")}
+          alt="Katsuko Saruhashi"
+          width={70}
+          height={70}
+        />
+        <ul>
+          <li>
+            <b>Profession: </b>
+            geochemist
+          </li>
+          <li>
+            <b>Awards: 2 </b>
+            (Miyake Prize for geochemistry, Tanaka Prize)
+          </li>
+          <li>
+            <b>Discovered: </b>a method for measuring carbon dioxide in seawater
+          </li>
+        </ul>
+      </section>
+    </div>
   );
-};
-
-export default function Poem() {
-  const poemList = poem.lines.map((line, index) => verseComponent(line, index));
-  return <article>{poemList}</article>;
 }
