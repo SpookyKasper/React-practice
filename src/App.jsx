c2a33bf70ab7be670e5dcbdc936b504ffb69e383
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-08-06 14:58:18 +0200

    Prevent next button to work when end of list is reached

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,38 @@
-import { getImageUrl } from "./utils.js";
-
-const Profile = ({
-  name,
-  profession,
-  imageId,
-  awardsNum,
-  awardsText,
-  discovery,
-  size = 70,
-}) => {
-  return (
-    <section className="profile">
-      <h2>{name}</h2>
-      <img
-        className="avatar"
-        src={getImageUrl(imageId)}
-        alt={name}
-        width={size}
-        height={size}
-      />
-      <ul>
-        <li>
-          <b>Profession: </b>
-          {profession}
-        </li>
-        <li>
-          <b>Awards: {awardsNum} </b>
-          {awardsText}
-        </li>
-        <li>
-          <b>Discovered: </b>
-          {discovery}
-        </li>
-      </ul>
-    </section>
-  );
-};
+import { useState } from "react";
+import { sculptureList } from "./data.js";
 
 export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
+  function handleNextClick() {
+    if (index === sculptureList.length - 1) {
+      console.log(index);
+      return;
+    }
+    setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
   return (
-    <div>
-      <h1>Notable Scientists</h1>
-      <Profile
-        name={"Maria Sklodowska-Curie"}
-        imageId={"szV5sdG"}
-        profession={"physicist and chemist"}
-        awardsNum={4}
-        awardsText={
-          "(Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)"
-        }
-        discovery={"polonium (chemical element)"}
-      />
-      <Profile
-        name={"Katsuko Saruhashi"}
-        imageId={"Yfe0qp2"}
-        profession={"geochemist"}
-        awardsNum={2}
-        awardsText={"(Miyake Prize for geochemistry, Tanaka Prize)"}
-        discovery={"a method for measuring carbon dioxide in seawater"}
-      />
-    </div>
+    <>
+      <button onClick={handleNextClick}>Next</button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
+    </>
   );
 }
