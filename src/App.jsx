0f1258466bf651767a6ae745bcea39f14b2a173c
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-08-07 23:31:09 +0200

    Implement multiple selection

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,45 @@
 import { useState } from "react";
+import { letters } from "./data.js";
+import Letter from "./Letter.js";
 
-export default function Clock(props) {
-  const [color, setColor] = useState(props.color);
-  return <h1 style={{ color: color }}>{props.time}</h1>;
+export default function MailClient() {
+  const [selectedIds, setSelectedIds] = useState([]);
+
+  // TODO: allow multiple selection
+  const selectedCount = 1;
+
+  function handleToggle(toggledId) {
+    if (selectedIds.includes(toggledId)) {
+      const selected = selectedIds.filter((id) => id !== toggledId);
+      setSelectedIds(selected);
+    } else {
+      // TODO: allow multiple selection
+      const selected = selectedIds.map((id) => id);
+      selected.push(toggledId);
+      setSelectedIds(selected);
+    }
+  }
+
+  return (
+    <>
+      <h2>Inbox</h2>
+      <ul>
+        {letters.map((letter) => (
+          <Letter
+            key={letter.id}
+            letter={letter}
+            isSelected={
+              // TODO: allow multiple selection
+              selectedIds.includes(letter.id)
+            }
+            onToggle={handleToggle}
+          />
+        ))}
+        <hr />
+        <p>
+          <b>You selected {selectedCount} letters</b>
+        </p>
+      </ul>
+    </>
+  );
 }
--- src/Needs.jsx
+++
@@ -1,3 +0,0 @@
-export function Needs() {
-  return <h2>My Needs</h2>;
-}
