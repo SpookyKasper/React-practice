2d2902788cd4387da14100f6abbcacc7dfc29d35
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-08-17 14:40:19 +0200

    Practice effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,46 @@
-import React from "react";
 import { useState, useEffect } from "react";
-import { initialTodos, createTodo } from "./todos.js";
+import React from "react";
 
-export default function TodoList() {
-  const [todos, setTodos] = useState(initialTodos);
-  const [showActive, setShowActive] = useState(false);
+export default function Form() {
+  const [showForm, setShowForm] = useState(false);
+  const [message, setMessage] = useState("");
 
-  const activeTodos = todos.filter((todo) => !todo.completed);
-  const visibleTodos = showActive ? activeTodos : todos;
+  function handleSubmit(e) {
+    sendMessage(message);
+    e.preventDefault();
+    setShowForm(false);
+  }
 
-  return (
-    <>
-      <label>
-        <input
-          type="checkbox"
-          checked={showActive}
-          onChange={(e) => setShowActive(e.target.checked)}
-        />
-        Show only active todos
-      </label>
-      <NewTodo onAdd={(newTodo) => setTodos([...todos, newTodo])} />
-      <ul>
-        {visibleTodos.map((todo) => (
-          <li key={todo.id}>
-            {todo.completed ? <s>{todo.text}</s> : todo.text}
-          </li>
-        ))}
-      </ul>
-      <footer>{activeTodos.length} todos left</footer>
-    </>
-  );
-}
-
-function NewTodo({ onAdd }) {
-  const [text, setText] = useState("");
-
-  function handleAddClick() {
-    setText("");
-    onAdd(createTodo(text));
+  if (!showForm) {
+    return (
+      <>
+        <h1>Thanks for using our services!</h1>
+        <button
+          onClick={() => {
+            setMessage("");
+            setShowForm(true);
+          }}
+        >
+          Open chat
+        </button>
+      </>
+    );
   }
 
   return (
-    <>
-      <input value={text} onChange={(e) => setText(e.target.value)} />
-      <button onClick={handleAddClick}>Add</button>
-    </>
+    <form onSubmit={handleSubmit}>
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+      />
+      <button type="submit" disabled={message === ""}>
+        Send
+      </button>
+    </form>
   );
+}
+
+function sendMessage(message) {
+  console.log("Sending message: " + message);
 }
