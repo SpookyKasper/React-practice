02ee4e5d16d50b6d189921fb98ef2a58c320edbf
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-08-24 20:01:18 +0200

    Add react

--- src/Letter.jsx
+++ src/Letter.jsx
@@ -1,27 +1,24 @@
-export default function Letter({
-  letter,
-  isHighlighted,
-  onHover,
-  onToggleStar,
-}) {
+import { useEffect, useState } from "react";
+import React from "react";
+
+const Image = () => {
+  const [imageURL, setImageURL] = useState(null);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos", { mode: "cors" })
+      .then((response) => response.json())
+      .then((response) => setImageURL(response[0].url))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
-    <li
-      className={isHighlighted ? "highlighted" : ""}
-      onFocus={() => {
-        onHover(letter);
-      }}
-      onPointerMove={() => {
-        onHover(letter);
-      }}
-    >
-      <button
-        onClick={() => {
-          onToggleStar(letter.id);
-        }}
-      >
-        {letter.isStarred ? "Unstar" : "Star"}
-      </button>
-      {letter.subject}
-    </li>
+    imageURL && (
+      <>
+        <h1>An image</h1>
+        <img src={imageURL} alt={"placeholder text"} />
+      </>
+    )
   );
-}
+};
+
+export default Image;
